11c58a6760fa5964e0437917ef28832b5e95023e
HolHermann <holhermann78@gmail.com>
2022-01-31 16:50:20 +0100

    ajout des items

---
+++ front/js/script.js
@@ -0,0 +1,29 @@
+class Kanap {
+  constructor(kanapItemps) {
+    Object.assign(this, kanapItemps);
+  }
+}
+
+fetch("http://localhost:3000/api/products")
+  .then(function (res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function (elementsAPI) {
+    for (let elementAPI of elementsAPI) {
+      let item = new Kanap(elementAPI);
+      document.getElementById(
+        "items"
+      ).innerHTML += `<a href="./product.html?id=${item._id}">
+                <article>
+                  <img src="${item.imageUrl}" alt="${item.altTxt}">
+                  <h3 class="productName">${item.name}</h3>
+                  <p class="productDescription">${item.description}</p>
+                </article>
+              </a>`;
+    }
+  })
+  .catch(function (err) {
+    console.alert("An error occurred");
+  });
